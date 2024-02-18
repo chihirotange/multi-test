48c32c902cb71cd6055a6c2b76e4db094fbfbad4
ai-chan <ohanasakuiroha@gmail.com>
2024-02-18 20:20:13 +0700

    implemented main menu ui

--- Source/MultiTest/Private/Core/MultiTestGameInstance.cpp
+++ Source/MultiTest/Private/Core/MultiTestGameInstance.cpp
@@ -1,7 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Core/MultiTestGameInstance.h"
+#include "Blueprint/UserWidget.h"
+
+bool UMultiTestGameInstance::LoadMainMenu_Implementation()
+{
+	if(!IsValid(MainMenuWidgetClass))
+	{
+		UE_LOG(LogTemp, Error, TEXT("No assigned main menu widget class in game instance!"));
+		return false;
+	}
+	UUserWidget* MainMenu = CreateWidget(this, MainMenuWidgetClass);
+	MainMenu->AddToViewport();
+	return true;
+}
 
 void UMultiTestGameInstance::Init()
 {
--- Source/MultiTest/Public/Core/MultiTestGameInstance.h
+++ Source/MultiTest/Public/Core/MultiTestGameInstance.h
@@ -4,17 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Interface/GenericMenuUI.h"
 #include "MultiTestGameInstance.generated.h"
 
 /**
  * 
  */
+
+class UUserWidget;
 UCLASS()
-class MULTITEST_API UMultiTestGameInstance : public UGameInstance
+class MULTITEST_API UMultiTestGameInstance : public UGameInstance, public IGenericMenuUI
 {
-	GENERATED_BODY()
+public:
+	virtual bool LoadMainMenu_Implementation() override;
 
 private:
+	GENERATED_BODY()
+
 	virtual void Init() override;
 
 	UFUNCTION(Exec)
@@ -22,4 +28,8 @@ private:
 
 	UFUNCTION(Exec)
 	void Join(const FString& IP);
+
+private:
+	UPROPERTY(EditAnywhere, Category = Widgets)
+	TSubclassOf<UUserWidget> MainMenuWidgetClass;
 };
